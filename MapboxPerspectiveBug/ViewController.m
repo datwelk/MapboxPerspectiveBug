ad0f4ac472b9b2d34afc192aa9b031748717b603
Damiaan Twelker <datwelk@me.com>
2019-04-29 11:03:46 +0100

    test

--- MapboxPerspectiveBug/ViewController.m
+++ MapboxPerspectiveBug/ViewController.m
@@ -8,16 +8,52 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@import Mapbox;
 
-@end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@implementation ViewController {
+    MGLMapView *_mapView;
+    UIButton *_toggle;
+    BOOL _is3D;
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
+    _mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    [_mapView setCenterCoordinate:CLLocationCoordinate2DMake(59.31, 18.06) zoomLevel:15 animated:NO];
+    [self.view addSubview:_mapView];
+    
+    _toggle = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    _toggle.backgroundColor = [UIColor blueColor];
+    _toggle.layer.cornerRadius = 10.0f;
+    _toggle.layer.masksToBounds = YES;
+    _toggle.contentEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10);
+    [_toggle setTitle:@"Toggle Perspective" forState:UIControlStateNormal];
+    [_toggle setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_toggle sizeToFit];
+    [_toggle addTarget:self action:@selector(togglePressed) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_toggle];
+    
+    _toggle.translatesAutoresizingMaskIntoConstraints = NO;
+    [_toggle.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-10.0].active = YES;
+    [_toggle.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+}
+
+#pragma mark -
+
+- (void)togglePressed
+{
+    MGLMapCamera *camera = _mapView.camera;
+    camera.pitch = _is3D ? 0.0 : 45.0;
+    [_mapView setCamera:camera
+           withDuration:1.0f
+animationTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]
+            edgePadding:UIEdgeInsetsMake(10, 10, 50, 10)
+      completionHandler:nil];
+    
+    _is3D = !_is3D;
+}
 
 @end
